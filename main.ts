829605f63c856b2b9f9d4081875eb6e702571898
gloriasteaching <47659579+gloriasteaching@users.noreply.github.com>
2023-01-28 20:19:12 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -157,6 +157,25 @@ forever(function () {
             ...................bbbbddddb3ba.
             .......................bbbaaaa..
             `, 50, -15)
-        pause(1000)
+    } else {
+        projectile = sprites.createProjectileFromSide(img`
+            . . . . . . . 6 . . . . . . . . 
+            . . . . . . 8 6 6 . . . 6 8 . . 
+            . . . e e e 8 8 6 6 . 6 7 8 . . 
+            . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+            . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+            e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+            e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+            e e 2 e 2 2 4 2 2 e e e c . . . 
+            e e e e 2 e 2 2 e e e c . . . . 
+            e e e 2 e e c e c c c . . . . . 
+            . c c c c c c c . . . . . . . . 
+            `, -11, -48)
     }
+    pause(1000)
 })
